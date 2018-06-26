4f69c8cca6400623538e1f6b069381e9daf6601d
faultfactory <32714474+faultfactory@users.noreply.github.com>
2018-06-26 18:10:26 -0400

    fixed error in initializing path

--- src/main.cpp
+++ src/main.cpp
@@ -202,223 +202,214 @@ int main() {
 
 	int lane = 1; 
 
-	double ref_vel = 49.5; 
+	double ref_vel = 49.5;
 
+	h.onMessage([&map_waypoints_x, &map_waypoints_y, &map_waypoints_s, &map_waypoints_dx, &map_waypoints_dy, &lane, &ref_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+																															  uWS::OpCode opCode) {
+		// "42" at the start of the message means there's a websocket message event.
+		// The 4 signifies a websocket message
+		// The 2 signifies a websocket event
+		//auto sdata = string(data).substr(0, length);
+		//cout << sdata << endl;
+		if (length && length > 2 && data[0] == '4' && data[1] == '2')
+		{
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                     uWS::OpCode opCode) {
-    // "42" at the start of the message means there's a websocket message event.
-    // The 4 signifies a websocket message
-    // The 2 signifies a websocket event
-    //auto sdata = string(data).substr(0, length);
-    //cout << sdata << endl;
-    if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+			auto s = hasData(data);
 
-      auto s = hasData(data);
+			if (s != "")
+			{
+				auto j = json::parse(s);
 
-      if (s != "") {
-        auto j = json::parse(s);
-        
-        string event = j[0].get<string>();
-        
-        if (event == "telemetry") {
-          // j[1] is the data JSON object
-          
-        	// Main car's localization Data
-          	double car_x = j[1]["x"];
-          	double car_y = j[1]["y"];
-          	double car_s = j[1]["s"];
-          	double car_d = j[1]["d"];
-          	double car_yaw = j[1]["yaw"];
-          	double car_speed = j[1]["speed"];
+				string event = j[0].get<string>();
 
-          	// Previous path data given to the Planner
-          	auto previous_path_x = j[1]["previous_path_x"];
-          	auto previous_path_y = j[1]["previous_path_y"];
-          	// Previous path's end s and d values 
-          	double end_path_s = j[1]["end_path_s"];
-          	double end_path_d = j[1]["end_path_d"];
+				if (event == "telemetry")
+				{
+					// j[1] is the data JSON object
 
-          	// Sensor Fusion Data, a list of all other cars on the same side of the road.
-          	vector<vector<double>> sensor_fusion = j[1]["sensor_fusion"];
+					// Main car's localization Data
+					double car_x = j[1]["x"];
+					double car_y = j[1]["y"];
+					double car_s = j[1]["s"];
+					double car_d = j[1]["d"];
+					double car_yaw = j[1]["yaw"];
+					double car_speed = j[1]["speed"];
 
-						int prev_size = previous_path_x.size();
-						/* 
-          	json msgJson; 
+					// Previous path data given to the Planner
+					auto previous_path_x = j[1]["previous_path_x"];
+					auto previous_path_y = j[1]["previous_path_y"];
+					// Previous path's end s and d values
+					double end_path_s = j[1]["end_path_s"];
+					double end_path_d = j[1]["end_path_d"];
 
-          	vector<double> next_x_vals;
-          	vector<double> next_y_vals;
-						*/
-						
+					// Sensor Fusion Data, a list of all other cars on the same side of the road.
+					vector<vector<double>> sensor_fusion = j[1]["sensor_fusion"];
 
-						// Create vector of new points to fil
-						vector<double> ptsx;
-						vector<double> ptsy;
-						 
-						// Find current car state; 
-						double ref_x = car_x; 
-						double ref_y = car_y;
-						double ref_yaw = deg2rad(car_yaw);
+					int prev_size = previous_path_x.size();
+					/* 
+					json msgJson; 
 
+					vector<double> next_x_vals;
+					vector<double> next_y_vals;
+					*/
 
-						// Assure tangency for the current sate. 
-						// if the previous was almost empty reset 
-						if(prev_size<2)
-						{
-							// Generate two points that align with current state; 
-							double prev_car_x = car_x - cos(car_yaw);
-							double prev_car_y = car_y - sin(car_y);
+					// Create vector of new points to fil
+					vector<double> ptsx;
+					vector<double> ptsy;
 
-							ptsx.push_back(prev_car_x);
-							ptsx.push_back(car_x);
+					// Find current car state;
+					double ref_x = car_x;
+					double ref_y = car_y;
+					double ref_yaw = deg2rad(car_yaw);
 
-							ptsy.push_back(prev_car_y);
-							ptsy.push_back(car_y);
-						}
-						else
-						{
-							ref_x=previous_path_x[prev_size-1];
-							ref_y=previous_path_y[prev_size-1];
+					// Assure tangency for the current sate.
+					// if the previous was almost empty reset
+					if (prev_size < 2)
+					{
+						// Generate two points that align with current state;
+						double prev_car_x = car_x - cos(car_yaw);
+						double prev_car_y = car_y - sin(car_yaw);
 
-							double ref_x_prev = previous_path_x[prev_size-2];
-							double ref_y_prev = previous_path_y[prev_size-2];
-							ref_yaw = atan2(ref_y-ref_y_prev,ref_x-ref_x_prev);
+						ptsx.push_back(prev_car_x);
+						ptsx.push_back(car_x);
 
-							ptsx.push_back(ref_x_prev);
-							ptsx.push_back(ref_x);
+						ptsy.push_back(prev_car_y);
+						ptsy.push_back(car_y);
+					}
+					else
+					{
+						ref_x = previous_path_x[prev_size - 1];
+						ref_y = previous_path_y[prev_size - 1];
 
-							ptsy.push_back(ref_y_prev);
-							ptsy.push_back(ref_y);
-						}
+						double ref_x_prev = previous_path_x[prev_size - 2];
+						double ref_y_prev = previous_path_y[prev_size - 2];
+						ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
 
-						int wPoints = 3;
-						int sIncrement = 30;
-						for (int i = 1; i <(wPoints+1);i++)
-						{
-							// This does not work unless you add lane to the function line
-							vector<double> next_wp = getXY(car_s+(sIncrement*i),(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);				
-							ptsx.push_back(next_wp[0]);
-							ptsy.push_back(next_wp[1]);
-						}
+						ptsx.push_back(ref_x_prev);
+						ptsx.push_back(ref_x);
 
-						// Coordinate rotation to vehicle frame
-						for(int i = 0; i<ptsx.size();i++)
-						{
-							
-							double shift_x = ptsx[i]-ref_x;
-							double shift_y = ptsy[i]-ref_y;
+						ptsy.push_back(ref_y_prev);
+						ptsy.push_back(ref_y);
+					}
 
-						 	ptsx[i]=(shift_x*cos(0-ref_yaw)-(shift_y)*sin(0-ref_yaw));
-             	ptsy[i]=(shift_x*sin(0-ref_yaw)+(shift_y)*cos(0-ref_yaw));
-						}
+					int wPoints = 3;
+					int sIncrement = 30;
+					for (int i = 1; i < (wPoints + 1); i++)
+					{
+						// This does not work unless you add lane to the function line
+						vector<double> next_wp = getXY(car_s + (sIncrement * i), (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+						ptsx.push_back(next_wp[0]);
+						ptsy.push_back(next_wp[1]);
+					}
 
-						tk::spline s;
-						s.set_points(ptsx,ptsy);
+					// Coordinate rotation to vehicle frame
+					for (int i = 0; i < ptsx.size(); i++)
+					{
 
-						// Create display spline. 
-						vector<double> next_x_vals;
-						vector<double> next_y_vals;
+						double shift_x = ptsx[i] - ref_x;
+						double shift_y = ptsy[i] - ref_y;
 
-						// Re-use prior path points that were not consumed by the car. 
+						ptsx[i] = (shift_x * cos(0 - ref_yaw) - (shift_y)*sin(0 - ref_yaw));
+						ptsy[i] = (shift_x * sin(0 - ref_yaw) + (shift_y)*cos(0 - ref_yaw));
+					}
 
-						for (int i = 0; i<previous_path_x.size();i++)
-						{
-							next_x_vals.push_back(previous_path_x[i]);
-							next_y_vals.push_back(previous_path_y[i]);
-						}
+					tk::spline s;
+					s.set_points(ptsx, ptsy);
 
-						// how to break the spline points; 
-						double target_x = 30.0;
-						double target_y = s(target_x);
-						double target_dist = sqrt((target_x*target_x)+(target_y*target_y));
-						
-						double x_add_on = 0; 
-						// Create configurable number for path points
-						int pathPtCount = 50; 
-						// Fill the rest of the points. 
-						for(int i  = 1; i <= pathPtCount-previous_path_x.size();i++)
-						{
-							double N = (target_dist/(0.02*ref_vel/2.24));
-							double x_point = x_add_on + (target_x)/N;
-							double y_point = s(x_point);
-										
+					// Create display spline.
+					vector<double> next_x_vals;
+					vector<double> next_y_vals;
 
-							x_add_on = x_point; 
+					// Re-use prior path points that were not consumed by the car.
 
-							double x_ref = x_point;
-							double y_ref = y_point; 
+					for (int i = 0; i < previous_path_x.size(); i++)
+					{
+						next_x_vals.push_back(previous_path_x[i]);
+						next_y_vals.push_back(previous_path_y[i]);
+					}
 
-							//transform system back to world coordinates after defining path spline
+					// how to break the spline points;
+					double target_x = 30.0;
+					double target_y = s(target_x);
+					double target_dist = sqrt((target_x * target_x) + (target_y * target_y));
 
-							x_point = (x_ref *cos(ref_yaw)-y_ref*sin(ref_yaw));
-							y_point = (x_ref *sin(ref_yaw)+y_ref*cos(ref_yaw));
+					double x_add_on = 0;
+					// Create configurable number for path points
+					int pathPtCount = 50;
+					// Fill the rest of the points.
+					for (int i = 1; i <= pathPtCount - previous_path_x.size(); i++)
+					{
+						double N = (target_dist / (0.02 * ref_vel / 2.24));
+						double x_point = x_add_on + (target_x) / N;
+						double y_point = s(x_point);
 
-							x_point+=ref_x;
-							y_point+=ref_y;
+						x_add_on = x_point;
 
-							next_x_vals.push_back(x_point);
-							next_y_vals.push_back(y_point);
+						double x_ref = x_point;
+						double y_ref = y_point;
 
-							// double dist_inc = 0.3;
-							// for (int i = 0; i < 50; i++)
-							// {
-							// 	// Per video we want to be targeting the next point along path, not the current
-							// 	double next_s = car_s + (i + 1) * dist_inc;
-							// 	// Vehicle center is 2 lanes from the 'double yellow' center of traffic
-							// 	// Lane is 4 meters wide
-							// 	double next_d = 6;
-							// 	// Convert those two values to an x and y point using getXY. Return is a vector of coords;
-							// 	vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+						//transform system back to world coordinates after defining path spline
 
-							// 	next_x_vals.push_back(xy.at(0));
-							// 	next_y_vals.push_back(xy.at(1));
-						}
+						x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+						y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
 
-						// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-          	msgJson["next_x"] = next_x_vals;
-          	msgJson["next_y"] = next_y_vals;
+						x_point += ref_x;
+						y_point += ref_y;
 
-          	auto msg = "42[\"control\","+ msgJson.dump()+"]";
+						next_x_vals.push_back(x_point);
+						next_y_vals.push_back(y_point);
 
-          	//this_thread::sleep_for(chrono::milliseconds(1000));
-          	ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-          
-        }
-      } else {
-        // Manual driving
-        std::string msg = "42[\"manual\",{}]";
-        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-      }
-    }
-  });
+					}
+					json msgJson;
+					// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+					msgJson["next_x"] = next_x_vals;
+					msgJson["next_y"] = next_y_vals;
 
-  // We don't need this since we're not using HTTP but if it's removed the
-  // program
-  // doesn't compile :-(
-  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data,
-                     size_t, size_t) {
-    const std::string s = "<h1>Hello world!</h1>";
-    if (req.getUrl().valueLength == 1) {
-      res->end(s.data(), s.length());
-    } else {
-      // i guess this should be done more gracefully?
-      res->end(nullptr, 0);
-    }
-  });
+					auto msg = "42[\"control\"," + msgJson.dump() + "]";
 
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-    std::cout << "Connected!!!" << std::endl;
-  });
+					//this_thread::sleep_for(chrono::milliseconds(1000));
+					ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+				}
+			}
+			else
+			{
+				// Manual driving
+				std::string msg = "42[\"manual\",{}]";
+				ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+			}
+		}
+	});
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
-                         char *message, size_t length) {
-    ws.close();
-    std::cout << "Disconnected" << std::endl;
-  });
+	// We don't need this since we're not using HTTP but if it's removed the
+	// program
+	// doesn't compile :-(
+	h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data,
+					   size_t, size_t) {
+		const std::string s = "<h1>Hello world!</h1>";
+		if (req.getUrl().valueLength == 1)
+		{
+			res->end(s.data(), s.length());
+		}
+		else
+		{
+			// i guess this should be done more gracefully?
+			res->end(nullptr, 0);
+		}
+	});
 
-  int port = 4567;
-  if (h.listen(port)) {
-    std::cout << "Listening to port " << port << std::endl;
+	h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+		std::cout << "Connected!!!" << std::endl;
+	});
+
+	h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
+						   char *message, size_t length) {
+		ws.close();
+		std::cout << "Disconnected" << std::endl;
+	});
+
+	int port = 4567;
+	if (h.listen(port))
+	{
+		std::cout << "Listening to port " << port << std::endl;
   } else {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
